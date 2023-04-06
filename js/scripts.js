bae6dffaf4f40ecbc4803ac49c89997b2d16a9e6
Fernando Viveros <fernandoviveros95@gmail.com>
2023-04-06 18:50:26 -0300

    Actualizacion de CSS para responsive. Primer modelo de CARRUSEL para productos. Agregado de imagen de logo al Header. Entre otros.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,28 @@
 /*  --------------------- Transparencia en la barra de navegación --------------------- */
 window.addEventListener('scroll', function() {
     document.getElementById("navbar").classList.toggle("solido", window.scrollY > 100);
+
+    if(window.scrollY > 100){
+       document.getElementById("logo").src="./images/logoNombre.png"
+    } else {
+        document.getElementById("logo").src="./images/logo.png"
+    }
+    
 });
+
+/*  --------------------- Despliegue menú navbar --------------------- */
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x")
+    navbar.classList.toggle("active");
+};
 
 /*  --------------------- Cambio boton activo nav bar --------------------- */
 let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header div nav ul li a");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
     sections.forEach(sec => {
@@ -18,8 +35,11 @@ window.onscroll = () => {
         if(top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove("active");
-                document.querySelector("header div nav ul li a[href*=" + id + "]").classList.add("active");
+                document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
             });
         };
     });
+
+    menuIcon.classList.remove("bx-x")
+    navbar.classList.remove("active");
 };
